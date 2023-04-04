2c4406ca922d27770fbb348a54ecd9bfc8bca9fc
nashie1004 <naocenar@gmail.com>
2023-04-04 19:12:18 +0800

    adding more styles

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -13,33 +13,40 @@ export default function Chat() {
           <>
             {
               savedUsers.length !== 0 ? (
-                savedUsers.map((item, i) => {
-                  const LOCAL_STORAGE = JSON.parse(localStorage.getItem('token'))
-                  const lower = LOCAL_STORAGE.name.localeCompare(item.name)
-                  let socketID = ''
-
-                  if (lower == -1){
-                    socketID = `${LOCAL_STORAGE.name}-${item.name}` 
-                  } else {
-                    socketID = `${item.name}-${LOCAL_STORAGE.name}`
-                  } //
-
-                  return (
-                    <div className='user-card' key={i}>
-                      <img src={item.profile} alt={item.name} />
-                      <p className='card-title'>{item.name}</p>
-                      <Link to={`/chat/${socketID}`} key={i}>
-                        SocketID: {socketID}
-                      </Link>
-                      <br/>
-                      <button 
-                        onClick={() => handleButton(item.name, true, item.profile)}
-                        className='danger-btn'>
-                        Remove User
-                      </button>
-                    </div>
-                  )
-                })
+                <div className="card-container">
+                  {
+                    savedUsers.map((item, i) => {
+                      const LOCAL_STORAGE = JSON.parse(localStorage.getItem('token'))
+                      const lower = LOCAL_STORAGE.name.localeCompare(item.name)
+                      let socketID = ''
+    
+                      if (lower == -1){
+                        socketID = `${LOCAL_STORAGE.name}-${item.name}` 
+                      } else {
+                        socketID = `${item.name}-${LOCAL_STORAGE.name}`
+                      } //
+    
+                      return (
+                        <div className='user-card' key={i}>
+                          <img src={item.profile} alt={item.name} />
+                          <div style={{color: '#69F5FF', fontSize: '.8rem'}}>
+                            Chat with 
+                            <br/>
+                            <Link to={`/chat/${socketID}`} key={i}
+                            className='card-title'>
+                              {item.name}
+                            </Link>
+                          </div>
+                          <button 
+                            onClick={() => handleButton(item.name, true, item.profile)}
+                            className='danger-btn'>
+                            Remove User
+                          </button>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
               ) : (
                 <>
                   <p>No Connected User yet</p>
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -9,27 +9,32 @@ export default function Users() {
       <p className='heading'>Connect with other users</p>
       {
         loggedIn ? (
-          allUsers.map((item, i) => {
-            return <div key={i} className='user-card'>
-              <img src={item.profile} alt={item.name} />
-              <p className='card-title'>{item.name}</p>
-              {
-                item.added ? (
-                  <button 
-                    onClick={() => handleButton(item.name, item.added, item.profile)}
-                    className='danger-btn'>
-                    Remove User
-                  </button>
-                ) : (
-                  <button 
-                    onClick={() => handleButton(item.name, item.added, item.profile)}
-                    className='add-btn'>
-                    Add User
-                  </button>
-                )
-              }
-            </div>
-          })
+          <div className="card-container">
+            {
+              allUsers.map((item, i) => {
+                return <div key={i} className='user-card'>
+                  <img src={item.profile} alt={item.name} />
+                  <p className='card-title'>{item.name}</p>
+                  {
+                    item.added ? (
+                      <button 
+                        onClick={() => handleButton(item.name, item.added, item.profile)}
+                        className='danger-btn'>
+                        Remove User
+                      </button>
+                    ) : (
+                      <button 
+                        onClick={() => handleButton(item.name, item.added, item.profile)}
+                        className='add-btn'>
+                        Add User
+                      </button>
+                    )
+                  }
+                </div>
+              })
+            }
+          </div>
+
         ) : (
           <>
             <p>Not Logged In -Users</p>
